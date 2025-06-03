9bc66114ae58f09e81f04d5c23fdb2bed946981a
Michael Bucari-Tovo <mbucari1@gmail.com>
2025-06-03 15:48:01 -0600

    Add extension for readong shapefile polygons

--- src/GEHistoricalImagery/OSGeo.GDAL/GDALExtensions.cs
+++ src/GEHistoricalImagery/OSGeo.GDAL/GDALExtensions.cs
@@ -1,4 +1,9 @@
-﻿namespace OSGeo.GDAL;
+﻿using LibMapCommon.Geometry;
+using LibMapCommon;
+using OSGeo.OGR;
+using OSGeo.OSR;
+
+namespace OSGeo.GDAL;
 
 [Flags]
 public enum GDAL_OF : uint
@@ -29,5 +34,63 @@ internal static class GDALExtensions
 	public static void SetGeoTransform(this Dataset dataset, GeoTransform transform)
 	{
 		dataset.SetGeoTransform(transform.Transformation);
+	}
+
+	public record ShapePolygon(Wgs1984Poly Polygon, Dictionary<string, string> Features);
+	public static IEnumerable<ShapePolygon> GetPolygons(this DataSource shp)
+	{
+		if (shp.GetLayerCount() == 0)
+			yield break;
+
+		using var t_sr = new SpatialReference("");
+		t_sr.ImportFromEPSG(Wgs1984.EpsgNumber);
+
+		for (int i = shp.GetLayerCount() - 1; i >= 0; i--)
+		{
+			using var layer = shp.GetLayerByIndex(i);
+			if (layer.GetGeomType() is not wkbGeometryType.wkbPolygon)
+				continue;
+
+			using var s_sr = layer.GetSpatialRef();
+			using var xForm = new CoordinateTransformation(s_sr, t_sr);
+
+			for (Feature? feature; (feature = layer.GetNextFeature()) is not null; feature.Dispose())
+			{
+				using var geometry = feature.GetGeometryRef();
+				using var ring = geometry.GetGeometryRef(0);
+				if (ring.GetGeometryType() is not wkbGeometryType.wkbLineString and not wkbGeometryType.wkbLinearRing)
+					continue;
+
+				var numPoints = ring.GetPointCount();
+				if (numPoints < 3)
+					continue;
+
+				var featureCount = feature.GetFieldCount();
+				var features = new Dictionary<string, string>(featureCount);
+				for (int f = 0; f < featureCount; f++)
+				{
+					using var field = feature.GetFieldDefnRef(f);
+					features[field.GetName()] = feature.GetFieldAsString(f);
+				}
+
+				var points = new Wgs1984[numPoints];
+				var point = new double[3];
+				for (int j = 0; j < numPoints; j++)
+				{
+					ring.GetPoint(j, point);
+					xForm.TransformPoint(point);
+					points[j] = new Wgs1984(point[0], point[1]);
+				}
+
+				if (points[0].Equals(points[^1]))
+				{
+					if (points.Length < 3)
+						continue;
+					Array.Resize(ref points, points.Length - 1);
+				}
+
+				yield return new ShapePolygon(new Wgs1984Poly(points), features);
+			}
+		}
 	}
 }
